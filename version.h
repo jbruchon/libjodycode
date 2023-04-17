8d98f705073c5a3721e5081f81087099c8711b25
Jody Bruchon <jody@jodybruchon.com>
2023-04-17 16:27:46 -0400

    v1.1 release

--- version.h
+++ version.h
@@ -4,7 +4,7 @@
 #ifndef LIBJODYCODE_VERSION_H
 #define LIBJODYCODE_VERSION_H
 
-#define VER "1.0"
-#define VERDATE "2023-04-09"
+#define VER "1.1"
+#define VERDATE "2023-04-17"
 
 #endif /* LIBJODYCODE_VERSION_H */
